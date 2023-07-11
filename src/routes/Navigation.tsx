03381e2ee9d8f25a4256300d83cf97b7504004bf
CrisPer1988 <norberto.cp@hotmail.com>
2023-07-10 21:30:15 -0300

    update merge cristian-aaron

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter } from "react-router-dom"
 import {Routes, Route, NavLink, Navigate} from "react-router-dom"
 import Home from "../pages/Home"
-import Prueba from "../pages/Prueba"
+import GeneralDescription from "../pages/GeneralDescription";
+import Dashboard from "../pages/Dashboard";
+import TechnologiesEmployed from "../pages/TechnologiesEmployed";
+import WebUsers from "../pages/WebUsers";
 
 
 const Navigation = () => {
-  const [show, setShow] = useState(true)
+  // const [show, setShow] = useState(true)
 
-  const handleClick = () => {
-    setShow(!show)
-  }
+  // const handleClick = () => {
+  //   setShow(!show)
+  // }
 
   return (
     <>
@@ -20,16 +23,27 @@ const Navigation = () => {
                <NavLink to="/home">Home</NavLink> 
             </li>
             <li>
-            <NavLink to="/prueba">Prueba</NavLink> 
+            <NavLink to="/general">General</NavLink> 
+            </li>
+            <li>
+            <NavLink to="/dashboard">Dashboard</NavLink> 
+            </li>
+            <li>
+            <NavLink to="/technologies">Technologies</NavLink> 
+            </li>
+            <li>
+            <NavLink to="/webUsers">Web Users</NavLink> 
             </li>
         </ul>
     </nav>
 
     <Routes>
         <Route path="home" element={<Home />} />
-        <Route path="prueba" element={<Prueba />} />
+        <Route path="general" element={<GeneralDescription />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="technologies" element={<TechnologiesEmployed />} />
+        <Route path="webUsers" element={<WebUsers />} />
         <Route path="/*" element={<Navigate to="home" replace />} />
-
     </Routes>
 
     </BrowserRouter>
